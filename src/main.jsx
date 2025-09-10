7633765fcf86e40fe996bb79a295bba6817f4170
manikandan <manikandan110305@gmail.com>
2025-09-10 13:46:06 +0530

    Fix package.json merge conflict

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/freelance-portfolio/">
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [tailwindcss(),
-  react()],
-})
+  base: "/freelance-portfolio/",
+  plugins: [tailwindcss(), react()],
+});
